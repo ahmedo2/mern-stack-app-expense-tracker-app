79354c25fb3905e9a93fd6c750e37f26d01a3c4f
Ahmed Oublihi <oublihi.a@gmail.com>
2020-03-01 14:48:21 +0100

    Adding The Back-End

---
+++ client/build/precache-manifest.0b8e3937bfa82c99279cd185093a7ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3bf2fa07df6b3579c8cf601b82775a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79b643a11667b7785351",
+    "url": "/static/css/main.5939efef.chunk.css"
+  },
+  {
+    "revision": "20f203ec1768a3fb4a3e",
+    "url": "/static/js/2.19ed0709.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.19ed0709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b643a11667b7785351",
+    "url": "/static/js/main.bc697f32.chunk.js"
+  },
+  {
+    "revision": "7f9b9e34df2c30545e13",
+    "url": "/static/js/runtime-main.1c0d9806.js"
+  }
+]);
